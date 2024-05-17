140509d0b86ec18656338f105d6dab44c82c2637
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-05-18 01:41:24 +0700

    add animation

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
 @export var speed:int
+@onready var animation = $AnimationPlayer
 
 func handleInput():
 	var moveDir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDir * speed
 
+func updateAnimation():
+	if velocity.length() == 0:
+		animation.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		if velocity.x > 0: direction = "Right"
+		if velocity.y < 0: direction = "Up"
+		animation.play("walk"+ direction)
+	
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	updateAnimation()
+	
